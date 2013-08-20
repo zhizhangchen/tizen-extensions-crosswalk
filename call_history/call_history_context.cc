4ea7dfba99d90dc4ead6e98e37b52ea528b5649f
John Chen <zhang.z.chen@intel.com>
2013-08-20 13:11:14 +0800

    Format some code

--- call_history/call_history_context.cc
+++ call_history/call_history_context.cc
@@ -5,7 +5,6 @@
 #include "call_history/call_history_context.h"
 #include "common/picojson.h"
 #include <dlfcn.h>
-#include "jsc_wrapper.h"
 CXWalkExtension* xwalk_extension_init(int32_t api_version) {
 
   return ExtensionAdapter<CallHistoryContext>::Initialize();
@@ -26,7 +25,6 @@ const char CallHistoryContext::name[] = "tizen.callhistory";
 // This will be generated from call_history_api.js.
 extern const char kSource_call_history_api[];
 
-typedef int (*jsc_init_t)();
 typedef char* (*add_change_listener_t)(void*);
 
 template <class FuncType>
@@ -53,47 +51,16 @@ void* load_lib(const char* path, int flag) {
 const char* CallHistoryContext::GetJavaScript() {
   load_lib("/usr/lib/libaccess-control-bypass.so", RTLD_NOW|RTLD_GLOBAL);
   jsc_handle = load_lib("/usr/lib/libjsc-wrapper.so", RTLD_NOW);
-  jsc_init_t init = loadFunc<jsc_init_t>("init_jsc");
-  if (init)
-    init();
   return kSource_call_history_api;
 }
 
 typedef void* (*handle_msg_t)(ContextAPI* api, const char* msg);
 void CallHistoryContext::HandleMessage(const char* message) {
-  /*picojson::value& v = *new picojson::value();
-
-  std::string err;
-  picojson::parse(v, message, message + strlen(message), &err);
-  if (!err.empty()) {
-    std::cout << "Ignoring message.\n";
-    return;
-  }
-  std::string cmd = v.get("cmd").to_str();*/
-  handle_msg_t handle_msg = loadFunc<handle_msg_t>("handleMessage");
+  handle_msg_t handle_msg = loadFunc<handle_msg_t>("handle_msg");
   handle_msg(api_, message);
-
 }
 void CallHistoryContext::HandleSyncMessage(const char* message) {
-  picojson::value& v = *new picojson::value();
-  printf("HandleSyncMessage\n");
-
-  std::string err;
-  /*picojson::parse(v, message, message + strlen(message), &err);
-  if (!err.empty()) {
-    std::cout << "Ignoring sysc message.\n";
-    return;
-  }
-  std::string cmd = v.get("cmd").to_str();*/
-  typedef void* (*handle_sync_msg_t)(ContextAPI* api, const char* msg);
-  handle_msg_t handle_sync_msg = loadFunc<handle_msg_t>("handleSyncMessage");
+  handle_msg_t handle_sync_msg = loadFunc<handle_msg_t>("handle_sync_msg");
   handle_sync_msg(api_, message);
-  /*if (cmd == "addChangeListener") {
-    printf("Calling add_change_listener...\n");
-    add_change_listener_t add_change_listener = loadFunc<add_change_listener_t>("add_change_listener");
-    if (add_change_listener)
-      api_->SetSyncReply(add_change_listener(api_));
-  } */
-
 }
 
--- call_history/jsc_wrapper.cc
+++ call_history/jsc_wrapper.cc
@@ -1,5 +1,4 @@
-extern "C" __attribute__((visibility("default"))) int init_jsc();
-extern "C" __attribute__((visibility("default"))) const char* add_change_listener(void*);
+static int init_jsc() __attribute__((constructor));
 #include <JavaScriptCore/JSObjectRef.h>
 #include <stdio.h>
 #include "plugin.h"
@@ -13,13 +12,55 @@ extern "C" __attribute__((visibility("default"))) const char* add_change_listene
 #include <Ecore.h>
 #include <iostream>
 #include <glib.h>
-#include "jsc_wrapper.h"
 #include "common/picojson.h"
+#include "common/extension_adapter.h"
 #include <wrt-plugins-tizen/common/JSUtil.h>
 static JSObjectPtr objectInstance;
 static JSGlobalContextRef gContext;
-std::string toString(const JSStringRef& arg)
+static PluginPtr plugin;
+static DPL::WaitableEvent* wait = new DPL::WaitableEvent;
+DPL::Thread* appThread = WrtDeviceApis::Commons::ThreadPool::getInstance().getThreadRef(WrtDeviceApis::Commons::ThreadEnum::APPLICATION_THREAD);
+static JSValueRef added_cb(JSContextRef context,
+                                      JSObjectRef function,
+                                      JSObjectRef thisObject,
+                                      size_t argumentCount,
+                                      const JSValueRef arguments[],
+                                      JSValueRef *exception);
+static JSValueRef addChangeListenerCallback(JSContextRef context,
+                                      JSObjectRef function,
+                                      JSObjectRef thisObject,
+                                      size_t argumentCount,
+                                      const JSValueRef arguments[],
+                                      JSValueRef *exception);
+static const JSStaticFunction listener_staticfuncs[] =
 {
+    { "onadded", added_cb, kJSPropertyAttributeReadOnly },
+    { "onchanged", addChangeListenerCallback, kJSPropertyAttributeReadOnly },
+    { "onremoved", addChangeListenerCallback, kJSPropertyAttributeReadOnly },
+    { NULL, NULL, 0 }
+};
+static const JSClassDefinition listener_def =
+{
+    0, // version
+    kJSClassAttributeNone, // attributes
+    "Listener", // className
+    NULL, // parentClass
+    NULL, // staticValues
+    listener_staticfuncs, // staticFunctions
+    NULL, // initialize
+    NULL, // finalize
+    NULL, // hasProperty
+    NULL, // getProperty
+    NULL, // setProperty
+    NULL, // deleteProperty
+    NULL, // getPropertyNames
+    NULL, // callAsFunction
+    NULL, // callAsConstructor
+    NULL, // hasInstance
+    NULL // convertToType
+};
+
+static std::string toString(const JSStringRef& arg) {
     Assert(arg);
     std::string result;
     size_t jsSize = JSStringGetMaximumUTF8CStringSize(arg);
@@ -37,7 +78,7 @@ std::string toString(const JSStringRef& arg)
     return result;
 }
 
-std::string toString(JSContextRef ctx, JSValueRef value) {
+static std::string toString(JSContextRef ctx, JSValueRef value) {
   Assert(ctx && value);
   std::string result;
   JSStringRef str = JSValueToStringCopy(ctx, value, NULL);
@@ -45,9 +86,11 @@ std::string toString(JSContextRef ctx, JSValueRef value) {
   JSStringRelease(str);
   return result;
 }
+
 JSValueRef addChangeListenerCallback (JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception){
   printf("#########################\nchange listener callback called!\n############################\n");
 }
+
 static JSValueRef added_cb(JSContextRef context,
                                       JSObjectRef function,
                                       JSObjectRef thisObject,
@@ -76,7 +119,6 @@ static JSValueRef find_cb(JSContextRef context,
       if (_reply_id != "")
         msg += (", \"_reply_id\": " + _reply_id);
       msg += "}";
-      printf("%s\n", msg.c_str());
       ((ContextAPI*)_api)->PostMessage(msg.c_str());
     }
     else 
@@ -85,172 +127,14 @@ static JSValueRef find_cb(JSContextRef context,
   return NULL;
 }
 
-static const JSStaticFunction listener_staticfuncs[] =
-{
-    { "onadded", added_cb, kJSPropertyAttributeReadOnly },
-    { "onchanged", addChangeListenerCallback, kJSPropertyAttributeReadOnly },
-    { "onremoved", addChangeListenerCallback, kJSPropertyAttributeReadOnly },
-    { NULL, NULL, 0 }
-};
-static const JSClassDefinition listener_def =
-{
-    0, // version
-    kJSClassAttributeNone, // attributes
-    "Listener", // className
-    NULL, // parentClass
-    NULL, // staticValues
-    listener_staticfuncs, // staticFunctions
-    NULL, // initialize
-    NULL, // finalize
-    NULL, // hasProperty
-    NULL, // getProperty
-    NULL, // setProperty
-    NULL, // deleteProperty
-    NULL, // getPropertyNames
-    NULL, // callAsFunction
-    NULL, // callAsConstructor
-    NULL, // hasInstance
-    NULL // convertToType
-};
-#include <Ecore.h>
-#include <sys/select.h>
-#include <fcntl.h>
-typedef int (*EcoreSelectType)(int nfds, fd_set *readfds, fd_set *writefds,
-                               fd_set *exceptfds, struct timeval *timeout);
-EcoreSelectType m_oldEcoreSelect;
-int EcoreSelectInterceptor(int nfds,
-                                 fd_set *readfds,
-                                 fd_set *writefds,
-                                 fd_set *exceptfds,
-                                 struct timeval *timeout)
-{
-    // We have to check error code here and make another try because of some
-    // glib error's.
-    fd_set rfds, wfds, efds;
-    memcpy(&rfds, readfds, sizeof(fd_set));
-    memcpy(&wfds, writefds, sizeof(fd_set));
-    memcpy(&efds, exceptfds, sizeof(fd_set));
-
-    int ret = m_oldEcoreSelect(nfds,
-                                                         readfds,
-                                                         writefds,
-                                                         exceptfds,
-                                                         timeout);
-
-    if (ret == -1) {
-        // Check each descriptor to see if it is valid
-        for (int i = 0; i < nfds; i++) {
-            if (FD_ISSET(i,
-                         readfds) ||
-                FD_ISSET(i, writefds) || FD_ISSET(i, exceptfds))
-            {
-                // Try to get descriptor flags
-                int result = fcntl(i, F_GETFL);
-
-                if (result == -1) {
-                    if (errno == EBADF) {
-                        // This a bad descriptor. Remove all occurrences of it.
-                        if (FD_ISSET(i, readfds)) {
-                            LogPedantic(
-                                "GLIB_LOOP_INTEGRATION_WORKAROUND: Bad read descriptor: "
-                                << i);
-                            FD_CLR(i, readfds);
-                        }
-
-                        if (FD_ISSET(i, writefds)) {
-                            LogPedantic(
-                                "GLIB_LOOP_INTEGRATION_WORKAROUND: Bad write descriptor: "
-                                << i);
-                            FD_CLR(i, writefds);
-                        }
-
-                        if (FD_ISSET(i, exceptfds)) {
-                            LogPedantic(
-                                "GLIB_LOOP_INTEGRATION_WORKAROUND: Bad exception descriptor: "
-                                << i);
-                            FD_CLR(i, exceptfds);
-                        }
-                    } else {
-                        // Unexpected error
-                        assert(0);
-                    }
-                }
-            }
-        }
-
-        LogPedantic(
-            "GLIB_LOOP_INTEGRATION_WORKAROUND: Bad read descriptor. Retrying with default select.");
-
-        //Retry with old values and return new error
-        memcpy(readfds, &rfds, sizeof(fd_set));
-        memcpy(writefds, &wfds, sizeof(fd_set));
-        memcpy(exceptfds, &efds, sizeof(fd_set));
-
-        // Trying to do it very short
-        timeval tm;
-        tm.tv_sec = 0;
-        tm.tv_usec = 10;
-
-        if (timeout) {
-            ret = select(nfds, readfds, writefds, exceptfds, &tm);
-        } else {
-            ret = select(nfds, readfds, writefds, exceptfds, NULL);
-        }
-    }
-
-    return ret;
-}
-gboolean iterate_ecore_main_loop (gpointer user_data) {
+static gboolean iterate_ecore_main_loop (gpointer user_data) {
     ecore_main_loop_iterate ();
     return TRUE;
 }
-void* start_ecore_main_loop(void*){
-//void start_ecore_main_loop(void*, Ecore_Thread *th) {
-  //DPL::MainSingleaton::instance();
-  //ecore_init();
-  printf("starting ecore main loop...\n");
-  ecore_init();
-  union ConvertPointer
-    {
-        Ecore_Select_Function pointer;
-        EcoreSelectType function;
-    } convert;
 
-    convert.pointer = ecore_main_loop_select_func_get();
-    m_oldEcoreSelect = convert.function;
-
-    ecore_main_loop_select_func_set(&EcoreSelectInterceptor);
-
-    // It is impossible that there exist watchers at this time
-    // No need to add watchers
-    LogPedantic("ECORE event handler registered");
-
-  ecore_main_loop_glib_integrate();
-  //ecore_main_loop_begin();
-  /*while (1) {
-    ecore_main_loop_iterate ();
-    //usleep(1000000);
-  }*/
-  //g_idle_add(iterate_ecore_main_loop, NULL);
-  g_timeout_add(100, iterate_ecore_main_loop, NULL);
-  printf("ecore main returned\n");
-}
-class EcoreMainThread: public DPL::Thread {
-protected:
-  int ThreadEntry() {
-    start_ecore_main_loop(NULL);
-  }
-};
-PluginPtr plugin;
-class APIThread: public DPL::Thread {
-protected:
-  int ThreadEntry() {
-  }
-};
-DPL::WaitableEvent* wait = new DPL::WaitableEvent;
-std::string id;
 static void deleteMessage(void* api, void* message) {
-  printf("Deleting message...\n");
+  if (message)
+    free(message);
 }
 
 static void processMessage(void* api, void* message) {
@@ -294,22 +178,17 @@ static void processSyncMessage(void* api, void* message) {
     if (!result)
       printf("failure to call addChangeListener\n");
     else  {
-      printf("Call addChangeListener succeed:%s\n", toString(gContext, result).c_str());
-      id = toString(gContext, result);
+      ((ContextAPI*)api)->SetSyncReply(toString(gContext, result).c_str());
     }
     wait->Signal();
 }
 
-DPL::Thread* appThread = WrtDeviceApis::Commons::ThreadPool::getInstance().getThreadRef(WrtDeviceApis::Commons::ThreadEnum::APPLICATION_THREAD);
-
 int init_jsc() {
-  //(new EcoreMainThread())->Run();
   appThread->Run();
-  start_ecore_main_loop(NULL);
-  //pthread_t ecore_main_thread;
-  //pthread_create(&ecore_main_thread, NULL,  start_ecore_main_loop, NULL);
-  //pthread_detach(ecore_main_thread);
-  //ecore_thread_run(start_ecore_main_loop, NULL, NULL, NULL);
+  printf("starting ecore main loop...\n");
+  ecore_init();
+  g_timeout_add(100, iterate_ecore_main_loop, NULL);
+  printf("ecore main returned\n");
   printf("loading callhistory lib\n");
   plugin =  Plugin::LoadFromFile("/usr/lib/wrt-plugins/tizen-callhistory/libwrt-plugins-tizen-callhistory.so");
   plugin->OnWidgetStart(0);
@@ -319,19 +198,19 @@ int init_jsc() {
   gContext = JSGlobalContextCreateInGroup(NULL, NULL);
   for (std::list<Plugin::ClassPtr>::iterator it = list->begin(); it != list->end(); it ++) {
     printf("calling class template of %s\n", (*it)->getName().c_str());
-    //JSClassRef classRef = static_cast<JSClassRef>(const_cast<JSObjectDeclaration::ClassTemplate>((*it)->getClassTemplate()));
     objectInstance = JavaScriptInterfaceSingleton::Instance().
               createObject(gContext, *it);
   }
   return 0;
 }
 
-void handleMessage(ContextAPI* api, const char* msg) {
+#define PUBLIC_EXPORT __attribute__((visibility("default")))
+#define EXTERN_C extern "C"
+EXTERN_C PUBLIC_EXPORT void handle_msg(ContextAPI* api, const char* msg) {
   appThread->PushEvent(api, &processMessage, &deleteMessage, (void*)strdup(msg));
 }
-void handleSyncMessage(ContextAPI* api, const char* msg){
-   printf("pushing event...\n");
+  
+EXTERN_C PUBLIC_EXPORT void handle_sync_msg(ContextAPI* api, const char* msg){
    appThread->PushEvent(api, &processSyncMessage, &deleteMessage, NULL);
    DPL::WaitForSingleHandle(wait->GetHandle());
-   api->SetSyncReply(id.c_str());
 }
